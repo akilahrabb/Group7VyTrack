4785a79dbaeccfd4999e59091bccf171cfb4fffb
Akilah <akilahrabb@gmail.com>
2021-09-13 11:39:54 -0400

    added login.feature and LoginStep. Modified CukesRunner.java, configuration.properties, and TestBase.java; also removed test package and moved subpackages to steps and runners as appropriate

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -4,21 +4,15 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    plugin = {
-            "html:target/cucumber-report.html",
-            "json:target/cucumber.json",
-            "rerun:target/rerun.txt"
-    },
-    features = "src/test/resources/features",
-    glue = "src/test/java/com/vytrack/steps",
-    dryRun = false,
-    tags = ""
-
+        plugin = {"html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/features",
+        glue = "src/test/java/com/vytrack/steps",
+        dryRun = false,
+        tags = ""
 )
-
-
 public class CukesRunner {
 }
